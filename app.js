1df5c53883108e8a813635dbf104ce7ce0f109e7
fazle <ryan7998@gmail.com>
2021-02-01 10:05:54 -0500

    inquirer added

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-var profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+// // var profileDataArgs = process.argv.slice(2);
+// // const [name, github] = profileDataArgs;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if(err) throw Error(err);
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if(err) throw Error(err);
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
+
+inquirer
+    .prompt([
+        {
+            type:'input',
+            name:'name',
+            message:'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers.name));
